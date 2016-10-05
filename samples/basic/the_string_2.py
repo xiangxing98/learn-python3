ed31d68ea19b1fe69a78bedec419655e59a1689d
Stone.Hou <xiangxing985529@163.com>
2016-10-05 11:45:13 +0800

    add the string_2.py

---
+++ samples/basic/the_string_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+s1 = 72
+s2 = 85
+r = (s2 - s1)/s1 * 100
+print('小明的成绩提升了%.1f%%' % r)
+
+a = int(input('请输入去年成绩：'))
+b = int(input('请输入今年成绩：'))
+c = ((b-a)/a)*100
+print("小明的成绩提升了:%.1f%%" %(c))
