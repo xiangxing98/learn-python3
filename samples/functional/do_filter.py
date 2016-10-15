355d87192219183ed3fbd978894c3e51d0bb7de1
xiangxing98 <xiangxing985529@163.com>
2016-10-15 17:08:08 +0800

    update filter and prime number

--- samples/functional/do_filter.py
+++ samples/functional/do_filter.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+#在一个list中，删掉偶数，只保留奇数，
 def is_odd(n):
     return n % 2 == 1
 
 L = range(100)
 
+print(L)
+
 print(list(filter(is_odd, L)))
 
+#把一个序列中的空字符串删掉
 def not_empty(s):
     return s and s.strip()
 
 print(list(filter(not_empty, ['A', '', 'B', None, 'C', '  '])))
+
+#Python内建的filter()函数用于过滤序列。
+
+#和map()类似，filter()也接收一个函数和一个序列。和map()不同的是，filter()把传入的函数依次作用于每个元素，然后根据返回值是True还是False决定保留还是丢弃该元素。
+
+#注意到filter()函数返回的是一个Iterator，也就是一个惰性序列，所以要强迫filter()完成计算结果，需要用list()函数获得所有结果并返回list。
+
--- samples/functional/prime_numbers.py
+++ samples/functional/prime_numbers.py
@@ -1,29 +1,65 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+# 打印1000以内的素数:
 def main():
     for n in primes():
         if n < 1000:
             print(n)
         else:
             break
+#由于primes()也是一个无限序列，所以调用时需要设置一个退出循环的条件：
 
+#先构造一个从3开始的奇数序列,这是一个生成器，并且是一个无限序列。
 def _odd_iter():
     n = 1
     while True:
         n = n + 2
         yield n
-
+#定义一个筛选函数
 def _not_divisible(n):
     return lambda x: x % n > 0
-
+#定义一个生成器，不断返回下一个素数
 def primes():
     yield 2
-    it = _odd_iter()
+    it = _odd_iter() # 初始序列
     while True:
-        n = next(it)
+        n = next(it) # 返回序列的第一个数
         yield n
-        it = filter(_not_divisible(n), it)
+        it = filter(_not_divisible(n), it)  # 构造新序列
+#这个生成器先返回第一个素数2，然后，利用filter()不断产生筛选后的新的序列。
+
 
 if __name__ == '__main__':
     main()
+
+#回数是指从左向右读和从右向左读都是一样的数，例如12321，909。请利用filter()滤掉非回数
+#源码
+def is_palindrome(n):
+   return str(n) == str(n)[::-1]
+# 测试:   
+output = filter(is_palindrome, range(1, 1000))
+print(list(output))
+
+#回数是指从左向右读和从右向左读都是一样的数，例如12321，909。请利用filter()滤掉非回数
+def is_palindrome2(n):
+    s = str(n)
+    i = int(len(s)/2)
+    if s[:-i] == s[:i-1:-1]:
+        return int(s)
+
+# 测试:
+output = filter(is_palindrome2, range(1, 1000))
+print(list(output))
+
+#基本思路：
+#输入数字转成字符串str()，然后反转[::-1]，再变成整数int()与原输入数字比较,返回TRUE/False。两者相等则是回数，反之不是。
+def is_palindrome3(n):
+    return int(str(n)[::-1]) == n 
+# 测试:
+output = filter(is_palindrome3, range(1, 1000))
+print(list(output))
+
+
+
+
+
